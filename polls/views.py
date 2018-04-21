86f518d1d990f5c679d1d36e5c1ee61e0f06266e
Daniela <dmzamorau@gmail.com>
2018-04-21 14:27:48 +0200

    Changing the views file from polls

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -13,3 +14,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+# Leave the rest of the views (detail, results, vote) unchanged
