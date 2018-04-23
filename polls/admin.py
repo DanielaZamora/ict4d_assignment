12c3cf024c03b7b8f5c934016eb406ebd3abfc2d
Daniela <dmzamorau@gmail.com>
2018-04-23 22:08:25 +0200

    Adding new features to the admin and views file for using the new model Topic

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Topic
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -8,12 +8,13 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
+        (None,               {'fields': ['related_topic']}), 
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+    list_display = ('related_topic','question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['related_topic']
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -4,14 +4,13 @@ from django.db import models
 from django.utils import timezone
 
 class Topic(models.Model):
-    topic_name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    topic_name = models.CharField(max_length=300, default='')
     def __str__(self):
         return self.topic_name
 
 class Question(models.Model):
-    related_topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default='NONE')
     question_text = models.CharField(max_length=200)
+    related_topic = models.ForeignKey(Topic, on_delete=models.CASCADE,default='')
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
