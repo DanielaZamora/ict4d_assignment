ee05a25374fcfe1d25913aaef01229c79c4e360c
Daniela <dmzamorau@gmail.com>
2018-04-23 09:56:33 +0200

    Adding new model to the app called Correct

--- polls/models.py
+++ polls/models.py
@@ -14,11 +14,6 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-    
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -27,4 +22,8 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-        
+class Correct(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    correct_answer = models.CharField(max_length=200)
+    def __str__(self):
+        return self.choice_text
